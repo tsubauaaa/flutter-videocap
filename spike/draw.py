e92a94ef40686d23bad5ca46a830c42d7b3aa6d5
ts-hirota <ts-hirota@kddi.com>
2021-07-20 22:53:56 +0900

    face landmark drawing ついか

---
+++ spike/draw.py
@@ -0,0 +1,27 @@
+import cv2
+import mediapipe as mp
+
+mp_drawing = mp.solutions.drawing_utils
+mp_face_mesh = mp.solutions.face_mesh
+
+drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+with mp_face_mesh.FaceMesh(
+        static_image_mode=True,
+        max_num_faces=1,
+        min_detection_confidence=0.5) as face_mesh:
+    image = cv2.imread("./aligned/frame_det_00_001600.bmp")
+    results = face_mesh.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+    if not results.multi_face_landmarks:
+        print("results are empty.")
+        exit()
+    annotated_image = image.copy()
+    for face_landmarks in results.multi_face_landmarks:
+        print("face_landmarks:", face_landmarks)
+        mp_drawing.draw_landmarks(
+            image=annotated_image,
+            landmark_list=face_landmarks,
+            connections=mp_face_mesh.FACE_CONNECTIONS,
+            landmark_drawing_spec=drawing_spec,
+            connection_drawing_spec=drawing_spec)
+    cv2.imwrite("./annotated_image.png", annotated_image)
