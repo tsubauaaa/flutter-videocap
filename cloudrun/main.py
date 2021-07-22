466b5df9840bdf2aee2d6f06a14e25d355e8aa2e
ts-hirota <ts-hirota@kddi.com>
2021-07-22 21:16:58 +0900

    pubsub messageチョックを入れた

--- cloudrun/main.py
+++ cloudrun/main.py
@@ -2,45 +2,69 @@ import os
 import shutil
 import subprocess
 import sys
-
+import base64
 from flask import Flask, request
 from google.cloud import storage
 
 app = Flask(__name__)
 
+BUCKET_NAME = "video"
 WORK_DIR = "/app/"
 PROCESSED_DIR = "/app/processed"
 
 
-def download_video(envelope):
+def download_video(file_name):
     client = storage.Client()
-    bucket = client.bucket("videos")
-    blob = bucket.blob(envelope["message"])
-    with open(WORK_DIR + envelope, "wb") as video:
-        blob.download_to_file(video)
+    bucket = client.bucket(BUCKET_NAME)
+    blob = bucket.blob(file_name)
+    with open(WORK_DIR + file_name, "wb") as f:
+        blob.download_to_file(f)
 
 
 @app.route("/openface", methods=["POST"])
 def openface():
     envelope = request.get_json()
-    print(f'Recieved {envelope}.')
+    print(f'recieved request: {envelope}')
+
+    if not envelope:
+        msg = "no Pub/Sub message received"
+        print(f"error: {msg}")
+        return f"Bad Request: {msg}", 400
+
+    if not isinstance(envelope, dict) or "message" not in envelope:
+        msg = "invalid Pub/Sub message format"
+        print(f"error: {msg}")
+        return f"Bad Request: {msg}", 400
+
+    pubsub_message = envelope["message"]
+
+    if isinstance(pubsub_message, dict) and "data" in pubsub_message:
+        file_name = base64.b64decode(
+            pubsub_message["data"]).decode("utf-8").strip()
+    else:
+        msg = "invalid Pub/Sub message format"
+        print(f"error: {msg}")
+        return f"Bad Request: {msg}", 400
+
+    print(f"file name is {file_name}")
 
     # if remove_dir exsists then remove
     if os.path.isdir(PROCESSED_DIR):
         shutil.rmtree(PROCESSED_DIR)
         print(f"removed {PROCESSED_DIR}")
 
-    download_video(envelope)
+    download_video(file_name)
 
-    cp = subprocess.run(["/app/OpenFace/build/bin/FeatureExtraction", "-f", WORK_DIR + envelope])
+    cp = subprocess.run(
+        ["/app/OpenFace/build/bin/FeatureExtraction", "-f", WORK_DIR + file_name])
     if cp.returncode != 0:
         print("FeatureExtraction failed.", file=sys.stderr)
 
-    print("Done.")
+    print("FeatureExtraction done.")
 
     return ("", 204)
+
 
 if __name__ == "__main__":
     PORT = int(os.getenv("PORT")) if os.getenv("PORT") else 8080
     app.run(host="127.0.0.1", port=PORT, debug=True)
-
